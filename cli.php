b3cea1f92f54d0bba61cef41c46ed1864580f22e
hideokamoto <hide.okamoto@digitalcube.jp>
2016-12-12 22:46:17 +0900

    add sdk version

--- cli.php
+++ cli.php
@@ -12,14 +12,24 @@ require 'aws.phar';
  */
 class WP_CLI_CloudFront extends WP_CLI_Command {
     private $version = "v0.1.0";
+    private $aws_sdk_version = "v3";
 
     /**
-     * Prints current version of the shifter/cli.
+     * Prints current version of the cli command.
      *
      * @when before_wp_load
      */
     public function version(){
         WP_CLI::line( $this->version );
+    }
+
+    /**
+     * Prints current version of AWS SDK.
+     *
+     * @when before_wp_load
+     */
+    public function sdk_version(){
+        WP_CLI::line( $this->aws_sdk_version );
     }
 }
 WP_CLI::add_command( 'cloudfront', 'WP_CLI_CloudFront' );
